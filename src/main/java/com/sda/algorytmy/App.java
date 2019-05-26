08c24d10bacff4d5ab0bdadcac0b5905771f6a6d
bart7801 <bart7801@o2.pl>
2019-05-26 15:56:56 +0100

    pobieranie danych z pliku tekstowego

---
+++ src/main/java/com/sda/algorytmy/App.java
@@ -0,0 +1,27 @@
+package com.sda.algorytmy;
+
+        import java.io.IOException;
+        import java.nio.file.Files;
+        import java.nio.file.Path;
+        import java.nio.file.Paths;
+        import java.util.List;
+
+public class App{
+
+    public static Path file = Paths.get("real_estate.csv");
+
+    public static void main(String[] args) {
+
+        try {
+            byte[] lines2 = Files.readAllBytes(file);
+            List<String> lines = Files.readAllLines(file);
+            System.out.println(lines);
+
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
